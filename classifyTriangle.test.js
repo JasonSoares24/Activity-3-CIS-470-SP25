12f11cfa0c075ca2dcfd1240ba98d6564cbbecfb
JasonSoares24 <jsoares12@umassd.edu>
2025-02-23 22:36:58 +0000

    Added 20 test cases

--- classifyTriangle.test.js
+++ classifyTriangle.test.js
@@ -4,17 +4,43 @@ const  classifyTriangle  = require('./classifyTriangle');
 describe('classifyTriangle: Required input conditions -  See readme for more details', () => {
     test('should classify an Equilateral triangle', () => {
       expect(classifyTriangle(10, 10, 10)).toBe('Equilateral');
-      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+      // Boundary Tests
+      expect(classifyTriangle(1, 1, 1)).toBe('Equilateral');
+      expect(classifyTriangle(199, 199, 199)).toBe('Equilateral');
+      expect(classifyTriangle(200, 200, 200)).toBe('Equilateral');
+      expect(classifyTriangle(2, 2, 2)).toBe('Equilateral');
+
+      // Equivalence Tests
+      expect(classifyTriangle(100, 100, 100)).toBe('Equilateral');
+      expect(classifyTriangle(50, 50, 50)).toBe('Equilateral');
+      expect(classifyTriangle(35, 35, 35)).toBe('Equilateral');
+      expect(classifyTriangle(70, 70, 70)).toBe('Equilateral');
     });
   
     test('should return error for invalid inputs', () => {
       expect(classifyTriangle(0, 10, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
-        // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+
+      // Boundary Testing  
+      expect(classifyTriangle(-1, 10, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(10, -1, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(-1, -1, -1)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(10, 10, 201)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(10, 201, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(201, 201, 201)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+
+      // Equivalence Testing
+      expect(classifyTriangle(-200, 10, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(10, 500, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+      expect(classifyTriangle(10, 10, 700)).toBe('Error: Input conditions C1, C2, or C3 failed.');
     });
   
     test('should return "Not a Triangle" for invalid triangle sides', () => {
       expect(classifyTriangle(1, 2, 3)).toBe('Not a Triangle');
-      // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
+      
+      // Equivalence Testing
+      expect(classifyTriangle(2, 5, 7)).toBe('Not a Triangle');
+      expect(classifyTriangle(1, 200, 1)).toBe('Not a Triangle');
+      expect(classifyTriangle(1, 1, 200)).toBe('Not a Triangle');
     });
   });
   
